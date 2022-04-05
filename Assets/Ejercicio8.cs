0d2b27fdeefd12a4aa3db46c8c219ddca0a38ccb
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-05 08:19:57 -0300

    r

---
+++ Assets/Ejercicio8.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio8 : MonoBehaviour {
+    public int dia;
+	// Use this for initialization
+	void Start () {
+        switch (dia)
+        {
+            case 1:
+                Debug.Log("Domingo");
+                break;
+            case 2:
+                Debug.Log("Lunes");
+                break;
+            case 3:
+                Debug.Log("Martes");
+                break;
+            case 4:
+                Debug.Log("Miercoles");
+                break;
+            case 5:
+                Debug.Log("Jueves");
+                break;
+            case 6:
+                Debug.Log("Viernes");
+                break;
+            case 7:
+                Debug.Log("Sabado");
+                break;
+            default:
+                Debug.Log("El dia ingresado no es valido");
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

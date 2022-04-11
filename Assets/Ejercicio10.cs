4cd0ccadeb9fbcc932e3eede5e853cd71bf50bf2
AndresChouela <101569013+AndresChouela@users.noreply.github.com>
2022-04-10 22:14:00 -0300

    ejercicio 10

--- Assets/Ejercicio10.cs
+++ Assets/Ejercicio10.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//10. Realizá un programa que resuelva el siguiente problema: Nos piden desarrollar un  programa que informe si un estacionamiento se encuentra o no abierto según la hora  ingresada por el usuario.  
+//Nosotros sabemos que el estacionamiento se encuentra abierto entre las 10 y las 18hs. (Crear constante HORA_APERTURA = 10, y HORA_CIERRE = 18).  
+//Al iniciar el programa, se tomará como hora actual el valor de una variable expuesta en el Inspector, y deberemos setear una  variable booleana llamada estaAbierto en true o false, según corresponda, e informar por  pantalla mediante la misma si el estacionamiento se encuentra abierto o no.Obs: En el caso de que el valor de la variable correspona a una hora menor a 0 o mayor a 24, mostrar el  mensaje: “Ha ingresado una hora incorrecta” 
 
 public class Ejercicio10 : MonoBehaviour {
-
+    public int hora;
+    int hora_apertura = 10;
+    int hora_cierre = 18;
+    public bool estaabierto;
 	// Use this for initialization
 	void Start () {
-		
+
+        if (hora <= 24 && hora >= 0)
+        {
+            if (hora <= 18 && hora >= 10)
+            {
+                estaabierto = true;
+                Debug.Log("El estacionaminteo esta abierto");
+            }
+            else if (hora > 18 && hora < 10)
+            {
+                Debug.Log("El estacionaminteo esta cerrado");
+            }
+        }
+        else
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+        }
 	}
 	
 	// Update is called once per frame
